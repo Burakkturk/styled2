8fc85274d9ccf986943c68705f9deedf3b642638
Burakkturk <burak1845turk@icloud.com>
2024-05-25 11:54:07 +0300

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import Container from "./components/styled/Container";
 import { GlobalStyles } from "./components/styled/GlobalStyles";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import { ThemeProvider } from "styled-components";
 
 const App = () => {
+  const styles = {
+    colors:{
+      primary:"#eee",
+      secondary:"#bebe"
+    },
+    breakpoints: {xs: "300px", sm: "500px", md: "700px"},
+    margin: {},
+    padding: {}
+  }
   return (
     <>
+    <ThemeProvider theme={styles}>
     <GlobalStyles/>
     <Container>
       <Nav/>
     <Header/>
     </Container>
+    </ThemeProvider>
      
     </>
   );
